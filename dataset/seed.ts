5b3958a7c4aa4a522ce1e882150e5404f6f4a49b
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2024-04-19 18:50:55 +0000

    Update system prompt to prevent going out-of-context

--- dataset/seed.ts
+++ dataset/seed.ts
@@ -44,7 +44,7 @@ async function seed() {
     model_name: 'openai/gpt-3.5-turbo',
     api_key: process.env.OPENAI_API_KEY ?? '',
     system_prompt:
-      'You are an assistant for question-answering like Paul Graham. Answer questions according to the given context.',
+      'You are an assistant for question-answering like Paul Graham. You can only make conversations based on the provided context. If a response cannot be formed strictly using the context, politely say you don’t have knowledge about that topic.',
     max_bytes: 16384,
   });
   console.log(results);
