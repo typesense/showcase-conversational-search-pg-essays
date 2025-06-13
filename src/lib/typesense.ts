bae413c409021de22bc393432f16c454efac06d1
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-06-13 13:38:57 +0300

    fix(lib): use prefixed env params for typesense

--- src/lib/typesense.ts
+++ src/lib/typesense.ts
@@ -1,17 +1,17 @@
-import { Client } from 'typesense';
+import { Client } from "typesense";
 
 const typesense = new Client({
   nodes: [
     {
-      host: process.env.TYPESENSE_HOST ?? 'localhost',
-      port: Number(process.env.TYPESENSE_PORT ?? 8108),
-      protocol: process.env.TYPESENSE_PROTOCOL ?? 'http',
+      host: process.env.NEXT_PUBLIC_TYPESENSE_HOST ?? "localhost",
+      port: Number(process.env.NEXT_PUBLIC_TYPESENSE_PORT ?? 8108),
+      protocol: process.env.NEXT_PUBLIC_TYPESENSE_PROTOCOL ?? "http",
     },
   ],
-  apiKey: process.env.TYPESENSE_SEARCH_API_KEY ?? '',
+  apiKey: process.env.NEXT_PUBLIC_TYPESENSE_SEARCH_API_KEY ?? "",
   // 15 minutes
   connectionTimeoutSeconds: 15 * 60,
-  logLevel: 'debug',
+  logLevel: "debug",
 });
 
 export default typesense;
